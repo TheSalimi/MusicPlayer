8262529c70b469df4424efe0b84e3b198a644801
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-10-18 15:37:59 +0330

    RunTime Permission

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,31 +1,57 @@
 package com.example.musicplayer
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.musicplayer.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        requestRunTimePermission()
 
-        shuffleButton.setOnClickListener{
-            Toast.makeText(this , "hey " , Toast.LENGTH_SHORT).show()
+        shuffleButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, PlayerActivity::class.java)
+            startActivity(intent)
         }
 
         favoriteButton.setOnClickListener {
-            val intent = Intent(this@MainActivity , FavoritesActivity::class.java)
+            val intent = Intent(this@MainActivity, FavoritesActivity::class.java)
             startActivity(intent)
         }
 
         playlistButton.setOnClickListener {
-            val intent = Intent(this@MainActivity , playlistActivity::class.java)
+            val intent = Intent(this@MainActivity, playlistActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun requestRunTimePermission(){
+        if(ActivityCompat.checkSelfPermission(this , android.Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this , arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),13)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==13){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this , "Perimission granted" , Toast.LENGTH_SHORT).show()
+            }
+            else{
+                ActivityCompat.requestPermissions(this , arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) , 13 )
+            }
         }
     }
 }
