268c2de490705da3b9342c1ea9cb45121d52d145
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-10-18 12:15:59 +0330

    :)

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.musicplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.musicplayer.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        shuffleButton.setOnClickListener{
+            Toast.makeText(this , "hey " , Toast.LENGTH_SHORT).show()
+        }
+
+        favoriteButton.setOnClickListener {
+            val intent = Intent(this@MainActivity , FavoritesActivity::class.java)
+            startActivity(intent)
+        }
+
+        playlistButton.setOnClickListener {
+            val intent = Intent(this@MainActivity , playlistActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
