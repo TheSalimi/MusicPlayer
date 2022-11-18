f1d4e36ef710fcdb34b3f13e3cd0c697600d834b
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-11-18 13:14:26 +0330

    Custom alert dialog with edit text

--- app/src/main/java/com/example/musicplayer/playlistActivity.kt
+++ app/src/main/java/com/example/musicplayer/playlistActivity.kt
@@ -1,10 +1,14 @@
 package com.example.musicplayer
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.musicplayer.databinding.ActivityPlaylistBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_playlist.*
 
@@ -13,7 +17,8 @@ class playlistActivity : AppCompatActivity() {
     private  lateinit var  PlayListsAdapter : PlayListViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_playlist)
+        binding = ActivityPlaylistBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val tempList = ArrayList<String>()
         tempList.add("salam")
         tempList.add("amir")
@@ -25,6 +30,7 @@ class playlistActivity : AppCompatActivity() {
         backFromPlayList.setOnClickListener{
             finish()
         }
+        addPlayListBtn.setOnClickListener { customAlertDialog() }
     }
 
     private fun initializeRecyclerView(tempList : ArrayList<String>){
@@ -33,6 +39,19 @@ class playlistActivity : AppCompatActivity() {
         playlists.layoutManager = GridLayoutManager(this@playlistActivity , 2)
         PlayListsAdapter = PlayListViewAdapter(this, tempList)
         playlists.adapter = PlayListsAdapter
+    }
+
+    private fun customAlertDialog(){
+        val customDialog = LayoutInflater.from(this@playlistActivity)
+            .inflate(R.layout.add_playlist_dialog , binding.root , false)
+
+        val builder = MaterialAlertDialogBuilder(this  , R.style.MyThemeOverlay_MaterialComponents_MaterialAlertDialog)
+
+        builder.setView(customDialog)
+            .setTitle("Playlist Details")
+            .setPositiveButton("Add"){ dialog , _ ->
+                dialog.dismiss()
+            }.show()
     }
 
 }
