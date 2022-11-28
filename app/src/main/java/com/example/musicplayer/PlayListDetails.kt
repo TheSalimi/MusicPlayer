b8301640488639c6da6ed0c062293f1a5c64f959
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-11-28 23:44:28 +0330

    fix refreshing music list  in PlayListDetails bug

--- app/src/main/java/com/example/musicplayer/PlayListDetails.kt
+++ app/src/main/java/com/example/musicplayer/PlayListDetails.kt
@@ -1,14 +1,17 @@
 package com.example.musicplayer
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.musicplayer.databinding.ActivityPlayListDetailsBinding
 import com.example.musicplayer.databinding.ActivityPlaylistBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_favorites.*
 import kotlinx.android.synthetic.main.activity_play_list_details.*
 
@@ -24,29 +27,44 @@ class PlayListDetails : AppCompatActivity() {
         binding = ActivityPlayListDetailsBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        binding.shuffleInDetils.visibility = View.INVISIBLE
         binding.backFromPlayListDetails.setOnClickListener { finish() }
 
         currentPlayListPosition = intent.extras?.get("index") as Int
         musics.setItemViewCacheSize(10)
         musics.setHasFixedSize(true)
         musics.layoutManager = LinearLayoutManager(this)
-
-        playlistActivity.musicPlayList.ref[currentPlayListPosition].playList.addAll(MainActivity.musicList)
-        playlistActivity.musicPlayList.ref[currentPlayListPosition].playList.shuffle()
         adapter = MusicAdapter( this , playlistActivity.musicPlayList.ref[currentPlayListPosition].playList , true)
+        binding.musics.adapter = adapter
         binding.playListName.isSelected = true
-        musics.adapter = adapter
 
         binding.shuffleInDetils.setOnClickListener {
             val intent = Intent(this , PlayerActivity::class.java)
-            intent.putExtra("index" , 8)
+            intent.putExtra("index" , 0)
             intent.putExtra("class" , "PlaylistDetailsShuffle")
             startActivity(intent)
         }
 
         binding.addBtn.setOnClickListener {
             startActivity(Intent( this , SelectionActivity::class.java))
+        }
+
+        binding.removeBtn.setOnClickListener {
+            val builder = MaterialAlertDialogBuilder(this)
+            builder.setTitle("Remove")
+                .setMessage("Do you want to remove all songs from playlist?")
+                .setPositiveButton("Yes") { dialog, _ ->
+                    playlistActivity.musicPlayList.ref[currentPlayListPosition].playList.clear()
+                    adapter.refreshPLayList()
+                    dialog.dismiss()
+                }
+                .setNegativeButton("No") { dialog, _ ->
+                    dialog.dismiss()
+                }
+            val customDialog = builder.create()
+            customDialog.show()
+            customDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.BLACK)
+            customDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.BLACK)
         }
     }
 
@@ -60,5 +78,6 @@ class PlayListDetails : AppCompatActivity() {
                 .into(binding.playListImageInDetails)
             binding.shuffleInDetils.visibility = View.VISIBLE
         }
+        adapter.notifyDataSetChanged()
     }
 }
