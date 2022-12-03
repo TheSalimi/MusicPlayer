cf3fe1cb540673309efbf4388dc0fbe28e80fb1d
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-12-03 23:50:24 +0330

    Handeling calls and other audio changes

--- app/src/main/java/com/example/musicplayer/MusicService.kt
+++ app/src/main/java/com/example/musicplayer/MusicService.kt
@@ -7,6 +7,7 @@ import android.app.Service
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Binder
 import android.os.Handler
@@ -17,11 +18,12 @@ import androidx.core.app.NotificationCompat
 import com.example.musicplayer.ApplicationClass.Companion.CHANNEL_ID
 import kotlinx.android.synthetic.main.activity_player.*
 
-class MusicService : Service() {
+class MusicService : Service()  , AudioManager.OnAudioFocusChangeListener {
     private var myBinder = MyBinder()
     var mediaPlayer: MediaPlayer? = null
     private lateinit var mediaSession: MediaSessionCompat
     private lateinit var runnable : Runnable
+    lateinit var audioManager: AudioManager
 
     override fun onBind(intent: Intent?): IBinder? {
         mediaSession = MediaSessionCompat(baseContext, "My Music")
@@ -144,5 +146,24 @@ class MusicService : Service() {
                 .postDelayed(runnable , 200)
         }
         Handler(Looper.getMainLooper()).postDelayed(runnable , 0)
+    }
+
+    override fun onAudioFocusChange(focusChange: Int) {
+        if(focusChange <= 0){
+            //pause music
+            PlayerActivity.binding.playPauseButton.setIconResource(R.drawable.ic_play)
+            NowPlaying.binding.IsPlayingPLayOrPause.setIconResource(R.drawable.ic_play)
+            showNotification(R.drawable.ic_play)
+            PlayerActivity.isPlaying = false
+            mediaPlayer!!.pause()
+        }
+        else{
+            //play music
+            PlayerActivity.binding.playPauseButton.setIconResource(R.drawable.ic_pause)
+            NowPlaying.binding.IsPlayingPLayOrPause.setIconResource(R.drawable.ic_pause)
+            showNotification(R.drawable.ic_pause)
+            PlayerActivity.isPlaying = true
+            mediaPlayer!!.start()
+        }
     }
 }
--- app/src/main/java/com/example/musicplayer/PlayerActivity.kt
+++ app/src/main/java/com/example/musicplayer/PlayerActivity.kt
@@ -2,9 +2,11 @@ package com.example.musicplayer
 
 import android.annotation.SuppressLint
 import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.graphics.Color
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.media.audiofx.AudioEffect
 import android.net.Uri
@@ -300,6 +302,8 @@ class PlayerActivity : AppCompatActivity(), ServiceConnection, MediaPlayer.OnCom
         musicService = binder.currentService()
         creatMediaPlayer()
         musicService!!.seekBarSetup()
+        musicService!!.audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        musicService!!.audioManager.requestAudioFocus(musicService , AudioManager.STREAM_MUSIC , AudioManager.AUDIOFOCUS_GAIN)
     }
 
     override fun onServiceDisconnected(name: ComponentName?) {
