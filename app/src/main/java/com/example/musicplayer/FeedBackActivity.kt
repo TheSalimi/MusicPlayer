bb855719a2ca307de4fea49e4c08fd637034c5bd
Amir Salimi <108394058+TheSalimi@users.noreply.github.com>
2022-12-04 23:29:34 +0330

    Update FeedBackActivity.kt

--- app/src/main/java/com/example/musicplayer/FeedBackActivity.kt
+++ app/src/main/java/com/example/musicplayer/FeedBackActivity.kt
@@ -2,9 +2,11 @@ package com.example.musicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.musicplayer.R
 import com.example.musicplayer.databinding.ActivityFeedBackBinding
 import com.example.musicplayer.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_feed_back.*
 
 class FeedBackActivity : AppCompatActivity() {
     private lateinit var  binding : ActivityFeedBackBinding
@@ -12,6 +14,13 @@ class FeedBackActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedBackBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val feedbackMsg = feedbackText.text.toString() + "\n" + feedbackEmail.text.toString()
+        val subject  = feedbackTopic.text.toString()
+
+        binding.feedbackSend.setOnClickListener {
+            Toast.makeText(this , feedbackMsg , Toast.LENGTH_LONG).show()
+        }
 
         supportActionBar?.title = "Feedback"
     }
